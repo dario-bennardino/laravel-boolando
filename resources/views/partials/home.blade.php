e7e6252575d686dad1018fdbe9d7b98ae257a553
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-09 21:05:50 +0200

    [add] esercizio completo con dati cards

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -2,15 +2,15 @@
     @foreach ($products as $product)
         <div class="product">
             <div class="top-product">
-                <img src="{{ $product['frontImage'] }}" alt="">
+                <img src="{{ $product['frontImage'] }}" alt="{{ $product['name'] }}">
 
-                <div v-if="product.isInFavorites" class="preferiti">&hearts;</div>
+                <div v-if="{{ $product['isInFavorites'] }}" class="preferiti">&hearts;</div>
             </div>
             <div class="bottom-product">
-                <div class="marca"></div>
-                <div class="articolo"></div>
+                <div class="marca">{{ $product['brand'] }}</div>
+                <div class="articolo">{{ $product['name'] }}</div>
                 <div class="prezzi">
-                    <div class="prezzo-pieno">€</div>
+                    <div class="prezzo-pieno">{{ $product['price'] }}€</div>
                 </div>
             </div>
         </div>
