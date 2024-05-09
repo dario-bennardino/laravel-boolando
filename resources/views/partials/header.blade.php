295db4705652a0f316541ee645e2b8eb023bd238
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-09 15:41:15 +0200

    [add] creata macro struttura

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="container-homepage">
+        <div class="topbar">
+            <div class="genere">
+                <a href="">Donna</a>
+                <a href="">Uomo</a>
+                <a href="">Bambino</a>
+            </div>
+            <div class="logo">
+                <img src="" alt="logo">
+            </div>
+            <div class="social">
+                <i class="fa-regular fa-user"></i>
+                <i class="fa-regular fa-heart"></i>
+                <i class="fa-solid fa-bag-shopping"></i>
+            </div>
+        </div>
+
+    </div>
+</header>
